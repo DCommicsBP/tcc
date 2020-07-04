9229b1805956ae90ce2fc85621f24daaceafd003
DCommicsBP <dayonne.p@gmail.com>
2020-07-03 22:48:31 -0300

    Arrumando a renderização do mapa

--- WhereIGo/App.tsx
+++ WhereIGo/App.tsx
@@ -1,28 +1,26 @@
 import React from 'react';
 import MapView from 'react-native-maps';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <MapView
-
-        initialRegion={{
-          latitude: -30.033056,
-          longitude: -51.230000,
-          latitudeDelta: 0.0042,
-          longitudeDelta: 0.0031
-        }}
-      />
+      <View style={styles.container}>
+        <MapView style={styles.mapStyle} />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  mapView: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0
-  }
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
