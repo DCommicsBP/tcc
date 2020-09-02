430e2991748fbb3c8568ab863384762c4d8fd6dc
DCommicsBP <dayonne.p@gmail.com>
2020-09-01 21:03:54 -0300

    adicionando mais alterações na seach box --- funcional, falta tirar o cinza bizarro

--- WhereIGo/app/pages/screens/Search.tsx
+++ WhereIGo/app/pages/screens/Search.tsx
@@ -1,10 +1,41 @@
 import * as React from "react";
 import SearchInput from "../../shared/inputs/SearchInput";
+import { View } from "react-native";
 
-export default function Search(){
-    return (
-        <SearchInput>
+export default class Search extends React.Component {
+    state = {
+        region: {
+          latitudeDelta: 0, latitude: 0, longitudeDelta: 0, longitude: 0
+        }
+      }
 
-        </SearchInput>
-    ) 
+
+    async componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+
+            ({ coords: { latitude, longitude } }) => {
+                this.setState({
+                    region: {
+                        latitudeDelta: 0, latitude: latitude, longitudeDelta: 0, longitude: longitude
+                    }
+                })
+            },
+            (error) => {
+            }, {
+            timeout: 2000,
+            enableHighAccuracy: true,
+            maximumAge: 1000,
+        });
+
+    }
+
+    render() {
+        let { region } = this.state;
+        return (
+            <View style={{ flex: 1 }}>
+                <SearchInput />
+                <SearchInput />
+            </View>
+        )
+    }
 }
--- WhereIGo/app/shared/inputs/SearchInput.tsx
+++ WhereIGo/app/shared/inputs/SearchInput.tsx
@@ -1,56 +1,48 @@
 import React, { Component } from 'react'; 
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'; 
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { Platform} from 'react-native' 
 
 export default class SearchInput extends Component {
     render (){
-        return  (<GooglePlacesAutocomplete
-        placeholder='Search'
-        minLength={2} // minimum length of text to search
-        autoFocus={false}
-        returnKeyType={'search'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
-        listViewDisplayed='auto'    // true/false/undefined
-        fetchDetails={true}
-        renderDescription={row => row.description} // custom description render
-        onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
-          console.log(data, details);
-        }}
-  
-        getDefaultValue={() => ''}
-  
-        query={{
+        return <GooglePlacesAutocomplete
+          placeholder="Para onde?"
+          onPress={()=>{}}
+          query={{
           // available options: https://developers.google.com/places/web-service/autocomplete
           key: 'AIzaSyBl_UJ_MQziKKhB-GB2MIVrXrhUwlX6IyY',
           language: 'en', // language of the results
           types: '(cities)' // default: 'geocode'
         }}
-  
+        
+        textInputProps={{
+          autoCapitalize:'none', 
+          autoCorrect: false
+        }}
+        fetchDetails
+        enablePoweredByContainer={false}
         styles={{
-          textInputContainer: {
-            width: '100%'
-          },
-          description: {
-            fontWeight: 'bold'
-          },
-          predefinedPlacesDescription: {
-            color: '#1faadb'
-          }
-        }}
-  
-        currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
-        currentLocationLabel="Current location"
-        nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
-        GoogleReverseGeocodingQuery={{
-          // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
-        }}
-        GooglePlacesSearchQuery={{
-          // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
-          rankby: 'distance',
-          types: 'bar'
-        }}
-  
-        filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+          container: {
+            position: 'relative', 
+            top: Platform.select({ios: 60, android: 40}), 
+            width: '100%', 
+            backgroundColor: 'white'
+            
+          }, 
+          textInput: {
+            height: 54, 
+            marginHorizontal: 20, 
+            borderTopWidth: 0, 
+            borderBottomWidth: 0, 
+            flex: 1, 
+            backgroundColor: 'white'
+          }, 
+          description:{
 
+          },
+          row:{
+
+          }
+        }} 
     />
-        )
    }
 }
