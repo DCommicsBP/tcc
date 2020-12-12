b3c362d73b37c33e736a79a4c60be250af6d3e02
DCommicsBP <dayonne.p@gmail.com>
2020-12-12 18:55:29 -0300

    adicionando as modificações do app - tudo funcional

--- WhereIGo/app/pages/screens/search-find/templates/TemplateCards.tsx
+++ WhereIGo/app/pages/screens/search-find/templates/TemplateCards.tsx
@@ -9,96 +9,104 @@ import Axios from "axios";
 export default function TemplateCards(props: any) {
 
     let placesSearchModel: any[] = [];
+    let breakPlaces: boolean = false; 
 
-    const [placesBreak, setPlacesBreak] = React.useState(false)
-    const [routeBreak, setRouteBreak] = React.useState(false)
-    const [placesSearch, setPlacesSearch] = React.useState(placesSearchModel)
+    let placesSearch: any[] = []; 
     const { origin, destiny, price, rating, kilometers, information } = props
-    const [route, setRoute] = React.useState([])
-    const [message, setMessage] = React.useState("Carregando...")
 
- 
-
-    const setCoordinates = (c: any) => {
-        props.newMarker(c)
-        console.log('cooordinates template ===> ', c)
-    }
-    const loadPlaces = function () {
+    const loadRoute = function () {
         let inf = '';
         information.forEach((element: any) => {
             inf += element + ","
         });
-        const [{ data, loading, error }, refetch] = useAxios("https://where-i-do-go-api-google-maps.herokuapp.com/search-places-controller/places-service/" + inf.substring(0, inf.length - 1) + "/" + origin.lng 
+
+        const [{ data, loading, error }, refetch] = useAxios("https://where-i-do-go-api-google-maps.herokuapp.com/search-places-controller/places-service/"
+        + inf.substring(0, inf.length - 1) + "/" + origin.lng
         + "/" + origin.lat + "/" + destiny.lng + "/" + destiny.lat + "/" + kilometers)
             if (data) {
-                let elements:any = []; 
+            let places: PlaceSearchModel [] = [];
               data.forEach((element: any) => {
-                let ele: any = element;
-                elements.push(ele)
+                let ele: PlaceSearchModel = element as PlaceSearchModel; 
+                    
+                places.push(ele);
                 })
                 
-                setPlacesSearch(elements)
-                setPlacesBreak(true);
-                
-                console.log('route axios hooks',  route)
+                placesSearch = places; 
+                breakPlaces = !breakPlaces; 
+                console.log('places search', placesSearch)
       }
+  
         if(error) {
             console.log('error places ==> ',  error)
         }
     }
-    /*
-    const gateSearch = () => {
-        React.useEffect(() => {
-            let inf = '';
-            information.forEach((element: any) => {
-                inf += element + ","
-            });
-            if(placesSearch.length > 0 && placesBreak) return; 
-            if(placesSearch.length <= 0){
-
-            Axios.get("https://where-i-do-go-api-google-maps.herokuapp.com/search-places-controller/places-service/" + inf.substring(0, inf.length - 1) + "/" + origin.lng + "/" + origin.lat + "/" + destiny.lng + "/" + destiny.lat + "/" + kilometers)
-                .then(response => {
-                    console.log(response)
-                    let data = response.data as any[]
-                    if (response.data != null && placesSearch.length <=0) {
-                        setPlacesSearch(response.data)
-                        setPlacesBreak(true)
-                        return; 
-                    }
-                })
-                .catch(error => {
-                    console.log('ERRROOOOO', error)
-                    setMessage("Não foi possível encontrar as informações no momento. Aguarde mais um instante. ")
-                })
-            }
-
-        }, [])
-
-    }*/
-
-    if(placesSearch.length == 0)
-        loadPlaces(); 
-
-return <ScrollView style={{}} horizontal pagingEnabled showsHorizontalScrollIndicator={false}  >
-        {placesSearch.length > 0 ? placesSearch.map((element: any, index: number) => {
-            console.log('element', element)
-            let place: PlaceSearchModel = {
-                address: element.address,
-                cep: element.cep,
-                city: element.city,
-                coordinates: element.coordinates,
-                country: element.country,
-                image: element.image,
-                name: element.name,
-                phone: element.phone,
-                rating: element.rating,
-                price: element.price,
-                state: element.state,
-                isClosed: false
-
-            }
-            return <Card key={index + ''} props={place} coordinates={(c: any) => setCoordinates(c)} />
-        }) : <View><Text>{message}</Text></View>}
-    </ScrollView>
+  
+    const setCoordinates = (c: any) => {
+        props.newMarker(c)
+        console.log('cooordinates template ===> ', c)
+    }
+if(!breakPlaces){
+//  fetchData()
+loadRoute()
 
 }
+ 
+    return    <ScrollView style={{}} horizontal pagingEnabled showsHorizontalScrollIndicator={false}  >
+    {placesSearch.length > 0 ? placesSearch.map((element: any, index: number) => {
+        console.log('element', element)
+        let place: PlaceSearchModel = {
+            address: element.address,
+            cep: element.cep,
+            city: element.city,
+            coordinates: element.coordinates,
+            country: element.country,
+            image: element.image,
+            name: element.name,
+            phone: element.phone,
+            rating: element.rating,
+            price: element.price,
+            state: element.state,
+            isClosed: false
+
+        }
+        return <Card key={index + ''} props={place} coordinates={(c: any) => setCoordinates(c)} />
+    }) : <View><Text>nao deus</Text></View>}
+</ScrollView>;    
+
+}
+
+/*
+        if(placesBreak) return;joioioili9i              
+        const [{ data, loading, error }, refetch] = useAxios("https://where-i-do-go-api-google-maps.herokuapp.com/search-places-controller/places-service/"            + inf.substring(0, inf.length - 1) + "/" + origin.lng
+            + "/" + origin.lat + "/" + destiny.lng + "/" + destiny.lat + "/" + kilometers)
+
+        if (placesSearch.length == 0) {
+            if (data) {
+                let elements: any = [];
+                data.forEach((element: any) => {
+                    let ele: any = element;
+                    elements.push(ele)
+                    placesSearchModel.push(ele)
+                })
+
+                setPlacesSearch(elements)
+                setPlacesBreak(true);
+
+                console.log('route axios hooks teste  estado dos bagulho', placesSearch)
+
+
+            
+
+            }
+        }
+
+
+        if (error)
+            return <View><Text>Não deu!</Text></View>
+
+        if (loading) {
+
+            console.log('Está carregando !!!')
+            return <View><Text>Carregando !!!</Text></View>
+geyryhfy8guv vfd fb fr gtyu
+        }*/
